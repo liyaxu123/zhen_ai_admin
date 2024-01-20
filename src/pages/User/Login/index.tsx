bcd7d319cd279807625fd9ec36e4f1984ef11353
liyaxu123 <2504862082@qq.com>
2024-01-20 18:33:37 +0800

    fixed: md5方法完善参数类型

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -9,7 +9,7 @@ import * as crypto from 'crypto';
 import React, { useRef, useState } from 'react';
 import { flushSync } from 'react-dom';
 
-function md5(str) {
+function md5(str: string) {
   const hash = crypto.createHash('md5');
   hash.update(str);
   return hash.digest('hex');
